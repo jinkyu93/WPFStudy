0628764dfa79bc930db26509009098937da2b026
jinkyu2005 <jinkyu2005@naver.com>
2019-01-29 23:06:05 +0900

    MVVM 패턴을 적용한 WPF 예제 프로젝트 작성

---
+++ WPFSample/Commons/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace WPFSample.Commons
+{
+	internal class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		internal void NotifyPropertyChanged(string prop)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(prop));
+			}
+		}
+	}
+}
---
+++ WPFSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows.Input;
+using WPFSample.Commons;
+
+namespace WPFSample.ViewModels
+{
+	internal class MainViewModel : BaseViewModel
+	{
+		#region Fields
+
+		private string _textBoxText;
+
+		private string _textBlockText;
+
+		#endregion // Fields
+
+		#region Properties
+
+		public string TextBoxText
+		{
+			get { return this._textBoxText; }
+			set
+			{
+				this._textBoxText = value;
+				this.NotifyPropertyChanged("TextBoxText");
+			}
+		}
+
+		public string TextBlockText
+		{
+			get { return this._textBlockText; }
+			set
+			{
+				if (this._textBlockText != value)
+				{
+					this._textBlockText = value;
+					this.NotifyPropertyChanged("TextBlockText");
+				}
+			}
+		}
+
+		#endregion // Properties
+
+		#region Commands
+
+		private ICommand _changeTextCommand;
+
+		public ICommand ChangeTextCommand => this._changeTextCommand ?? (this._changeTextCommand = new RelayCommand(p => this.ChangeText()));
+
+		#endregion // Commands
+
+		#region Constructors
+
+		public MainViewModel()
+		{
+			TextBoxText = string.Empty;
+			TextBlockText = "start";
+		}
+
+		#endregion // Constructors
+
+		#region Methods
+
+		private void ChangeText()
+		{
+			TextBlockText = TextBoxText;
+			TextBoxText = string.Empty;
+		}
+
+		#endregion // Methods
+	}
+}
